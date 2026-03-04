13c2f07eae444876ceec7839485f5518c53806c4
Oluwatob-i <tobiifemii@gmail.com>
2026-03-04 20:36:46 +0000

    chains: add chain ID constants

---
+++ chains/chains.go
@@ -0,0 +1,12 @@
+package chains
+
+// list of chains we currently have configs for
+const (
+	Mainnet    = 1
+	Arbitrum   = 42161
+	Base       = 8453
+	Katana     = 747474
+	Pulsechain = 369
+	Celo       = 42220
+	BSC        = 56
+)
